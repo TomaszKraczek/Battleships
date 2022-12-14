aa2891f21100c1955ac694a8fe82f823c7cee230
jkuca <46653059+jkuca@users.noreply.github.com>
2022-12-14 11:36:18 +0100

    working version

--- battleship/src/main/java/org/battleship/board/BoardFactory.java
+++ battleship/src/main/java/org/battleship/board/BoardFactory.java
@@ -1,8 +1,30 @@
 package org.battleship.board;
 
+
+import org.battleship.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class BoardFactory {
+    Random random = new Random();
+
+    private int X;
+    private int y;
+    public enum Direction{
+        LEFT,
+        UP,
+        RIGHT,
+        DOWN;
+    }
+
     public void randomPlacement(Board board) {
+
+        List<Integer> XandY = new ArrayList<>();
+        XandY = random.generateRandomXandY();
+
     }
     public void manualPlacement(Board board) {
     }
 }
+
--- battleship/src/main/java/org/battleship/user/Input.java
+++ battleship/src/main/java/org/battleship/user/Input.java
@@ -1,12 +1,15 @@
 package org.battleship.user;
 
 import java.util.Scanner;
+import org.battleship.board.Board;
+import org.battleship.ship.Square;
 public class Input {
 
     private String SelectedField;
     private int userChoice;
 
-
+    Board board = new Board();
+    Square[][] ocean = board.getBoard();
     private boolean validate(){
         return true;
     }
@@ -22,5 +25,13 @@ public class Input {
         int firstCoordinate = position.charAt(0) - 65;
         int secondCoordinate = Integer.parseInt(coordinates[1]) - 1;
         return new int[]{firstCoordinate, secondCoordinate};
+    }
+
+    public boolean checkIfFieldIsEmpty(int x, int y)
+    {
+        if (ocean[x][y].getSquareStatus() == "🟦"){
+            return true;
+        }
+        return false;
     }
 }
