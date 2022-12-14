cf7ebd56bd33db4082537b87e739375ea9ad1879
Tomasz Kraczek <tomek6363@gmail.com>
2022-12-14 10:34:32 +0100

    Putting ships on board with static method.

--- battleship/src/main/java/org/battleship/Game.java
+++ battleship/src/main/java/org/battleship/Game.java
@@ -31,40 +31,44 @@ public class Game {
             System.out.println("podaj kierunek rozstawienia");
             String choice = scan.next();
 
-            switch (choice) {
-                case "left":
-                    for (int i = 0; i < ship.getType().getShipSize(); i++) {
-                        Square partCoord = new Square(first, second - i, SquareStatus.SHIP);
-                        ship.addPartOfShip(partCoord);
-                    }
-                    board1.addShipToBoard(ship);
-                    break;
-                case "right":
-                    for (int i = 0; i < ship.getType().getShipSize(); i++) {
-                        Square partCoord = new Square(first, second + i, SquareStatus.SHIP);
-                        ship.addPartOfShip(partCoord);
-                    }
-                    board1.addShipToBoard(ship);
-                    break;
-                case "up":
-                    for (int i = 0; i < ship.getType().getShipSize(); i++) {
-                        Square partCoord = new Square(first - i, second, SquareStatus.SHIP);
-                        ship.addPartOfShip(partCoord);
-                    }
-                    board1.addShipToBoard(ship);
-                    break;
-                case "down":
-                    for (int i = 0; i < ship.getType().getShipSize(); i++) {
-                        Square partCoord = new Square(first + i, second, SquareStatus.SHIP);
-                        ship.addPartOfShip(partCoord);
-                    }
-                    board1.addShipToBoard(ship);
-                    break;
-            }
+            setShipOnBoard(board1, ship, first, second, choice);
 
         }
         monitor.displayBoard(board1);
 
+    }
+
+    private static void setShipOnBoard(Board board1, Ship ship, int first, int second, String choice) {
+        switch (choice) {
+            case "left":
+                for (int i = 0; i < ship.getType().getShipSize(); i++) {
+                    Square partCoord = new Square(first, second - i, SquareStatus.SHIP);
+                    ship.addPartOfShip(partCoord);
+                }
+                board1.addShipToBoard(ship);
+                break;
+            case "right":
+                for (int i = 0; i < ship.getType().getShipSize(); i++) {
+                    Square partCoord = new Square(first, second + i, SquareStatus.SHIP);
+                    ship.addPartOfShip(partCoord);
+                }
+                board1.addShipToBoard(ship);
+                break;
+            case "up":
+                for (int i = 0; i < ship.getType().getShipSize(); i++) {
+                    Square partCoord = new Square(first - i, second, SquareStatus.SHIP);
+                    ship.addPartOfShip(partCoord);
+                }
+                board1.addShipToBoard(ship);
+                break;
+            case "down":
+                for (int i = 0; i < ship.getType().getShipSize(); i++) {
+                    Square partCoord = new Square(first + i, second, SquareStatus.SHIP);
+                    ship.addPartOfShip(partCoord);
+                }
+                board1.addShipToBoard(ship);
+                break;
+        }
     }
 
 }
