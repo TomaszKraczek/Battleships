3a502e3df5e1e7384c3c22dbf6dfb09a6967eb54
Tomasz Kraczek <tomek6363@gmail.com>
2022-12-15 18:57:00 +0100

    Clean up Battleship class

--- battleship/src/main/java/org/battleship/Battleship.java
+++ battleship/src/main/java/org/battleship/Battleship.java
@@ -1,17 +1,7 @@
 package org.battleship;
-
-import org.battleship.board.Board;
-import org.battleship.ship.Ship;
-import org.battleship.ship.ShipType;
-import org.battleship.ship.Square;
-import org.battleship.ship.SquareStatus;
 import org.battleship.user.Input;
-import org.battleship.user.Player;
 import org.battleship.view.Display;
-import org.w3c.dom.ls.LSOutput;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Battleship {
     private final static Display monitor = new Display();
